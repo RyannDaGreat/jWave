b77d7d783af7dfe5641a90dbc12fd7ce636abc9c
RyanBurgert219 <sqrtryan@gmail.com>
2017-06-23 23:08:49 -0400

    setting up big endian encoding, or whatever the hell that is to get better audio quality

--- src/SynthTest2.java
+++ src/SynthTest2.java
@@ -1,52 +1,51 @@
-// import javax.sound.sampled.AudioFormat;
-// import javax.sound.sampled.AudioSystem;
-// import javax.sound.sampled.LineUnavailableException;
-// import javax.sound.sampled.SourceDataLine;
-// public class SynthTest2
-// {
-//     //
-//     protected static final int SAMPLE_RATE=44100;//16*1024;
-//     // public static byte[] createSinWaveBuffer(double freq,int ms)
-//     // {
-//     //     int samples=ms*SAMPLE_RATE/1000;
-//     //     byte[] output=new byte[samples];
-//     //     //
-//     //     double period=SAMPLE_RATE/freq;
-//     //     for(int i=0;i<output.length;i++)
-//     //     {
-//     //         double angle=2.0*Math.PI*i/period;
-//     //         output[i]=(byte)(Math.sin(angle)*127f);
-//     //     }
-//     //     return output;
-//     // }
-//     public static int[] createSinWaveBuffer(double freq,int ms)
-//     {
-//         int samples=ms*SAMPLE_RATE/1000;
-//         int[] output=new int[samples];
-//         //
-//         double period=SAMPLE_RATE/freq;
-//         for(int i=0;i<output.length;i++)
-//         {
-//             double angle=2.0*Math.PI*i/period;
-//             output[i]=(byte)(Math.sin(angle)*127f);
-//         }
-//         return output;
-//     }
-//     public static void main(String[] args) throws LineUnavailableException
-//     {
-//         final AudioFormat af=new AudioFormat(SAMPLE_RATE,16,1,true,true);
-//         SourceDataLine line=AudioSystem.getSourceDataLine(af);
-//         line.open(af,SAMPLE_RATE);
-//         line.start();
-//         boolean forwardNotBack=true;
-//         for(double freq=400;freq<=800;)
-//         {
-//             byte[] toneBuffer=createSinWaveBuffer(freq,50);
-//             int count=line.write(toneBuffer,0,toneBuffer.length);
-//             System.out.println(count);
-//             freq+=20;
-//         }
-//         line.drain();
-//         line.close();
-//     }
-// }
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+@SuppressWarnings("WeakerAccess")
+public class SynthTest2
+{
+    protected static final int SAMPLE_RATE=44100;//16*1024;
+    public static byte[] createSinWaveBuffer(double freq,int ms)
+    {
+        int samples=ms*SAMPLE_RATE/1000;
+        double[] output=new double[samples];
+        //
+        double period=SAMPLE_RATE/freq;
+        for(int i=0;i<output.length;i++)
+        {
+            double angle=2.0*Math.PI*i/period;
+            output[i]=Math.sin(angle);
+        }
+        return rOutpost.doublesTo32BitAudioBytes(output);
+    }
+    static final int bitsPerSample=16;//8⟷Byte，16⟷Short，32⟷Int
+    static final AudioFormat af=new AudioFormat(SAMPLE_RATE,bitsPerSample,1,true,true);
+    static SourceDataLine line;
+    static
+    {
+        try
+        {
+            line=AudioSystem.getSourceDataLine(af);
+            line.open(af,SAMPLE_RATE);
+            line.start();
+        }
+        catch(LineUnavailableException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws LineUnavailableException
+    {
+        for(double freq=400;freq<=800;)
+        {
+            byte[] toneBuffer=createSinWaveBuffer(freq,50);
+            int count=line.write(toneBuffer,0,toneBuffer.length);
+            System.out.println(count);
+            freq+=20;
+        }
+        line.drain();
+        line.close();
+    }
+}
