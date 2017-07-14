4534512cd29667ffafc00170f3fcfc03b9713299
RyanBurgert219 <sqrtryan@gmail.com>
2017-07-14 03:57:07 -0400

    added buffersize option so now you can use it in realtime

--- src/SynthTest.java
+++ src/SynthTest.java
@@ -5,7 +5,8 @@ import javax.sound.sampled.SourceDataLine;
 @SuppressWarnings("WeakerAccess")
 public class SynthTest
 {
-    protected static final int SAMPLE_RATE=441000;//16*1024;
+    protected static final int SAMPLE_RATE=44100;//16*1024;
+    static final int bufferSize=1024;//Determines latency! Lower -> Faster response but glitchier audio
     static final int bitsPerSample=16;//8⟷Byte，16⟷Short，32⟷Int. It appears that, for some reason, trying to use 32 bit causes some audio error. I don't know why.
     static final AudioFormat af=new AudioFormat(SAMPLE_RATE,bitsPerSample,1,true,true);
     static SourceDataLine line;
@@ -14,7 +15,7 @@ public class SynthTest
         try
         {
             line=AudioSystem.getSourceDataLine(af);
-            line.open(af,1024);
+            line.open(af,bufferSize);
             line.start();
         }
         catch(LineUnavailableException e)
@@ -36,9 +37,9 @@ public class SynthTest
         {
             saw.setPitch((Math.cos(r.toc()*.25)+1)*35-24);
             // System.out.println(saw.x);
-            int numberOfSamples=1024;
+            int numberOfSamples=bufferSize/bitsPerSample*8;
             // r.delay(1/SAMPLE_RATE);
-           line.write(saw.get16BitBuffer(numberOfSamples,SAMPLE_RATE),0,numberOfSamples*2);//numberOfSamples*2 because 16 bit buffer
+           line.write(saw.get16BitBuffer(numberOfSamples,SAMPLE_RATE),0,numberOfSamples*bitsPerSample/8);//numberOfSamples*2 because 16 bit buffer
             // line.write(saw.get16BitBuffer(numberOfSamples,SAMPLE_RATE),0,numberOfSamples*2);//numberOfSamples*2 because 16 bit buffer
         }
         // line.drain();
